97d97adbc434e73658e2ac1db12f3519cc032229
Cesaradu <424321999@qq.com>
2017-05-09 17:07:43 +0800

    视频登录页

--- CutHand/Login/Controller/LoginViewController.m
+++ CutHand/Login/Controller/LoginViewController.m
@@ -8,8 +8,17 @@
 
 #import "LoginViewController.h"
 #import "MainTabBarController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface LoginViewController ()
+
+@property (nonatomic, strong) MPMoviePlayerController *moviePlayerController;
+@property (nonatomic, strong) UIImageView *logoImage;
+@property (nonatomic, strong) UIButton *wechatBtn;
+@property (nonatomic, strong) UIButton *phoneBtn;
+@property (nonatomic, strong) UILabel *loginLabel;
+@property (nonatomic, strong) UILabel *slogen;
+
 
 @end
 
@@ -37,51 +46,100 @@
 }
 
 - (void)buildUI {
-    UIImageView *bgImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"login_bg"]];
-    bgImage.frame = self.view.frame;
-    [self.view addSubview:bgImage];
     
-    UIImageView *logoImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
-    [self.view addSubview:logoImage];
-    [logoImage mas_makeConstraints:^(MASConstraintMaker *make) {
+    //背景图
+//    UIImageView *bgImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"login_bg"]];
+//    bgImage.frame = self.view.frame;
+//    [self.view addSubview:bgImage];
+    
+    //视频播放
+    NSString *moviePath = [[NSBundle mainBundle] pathForResource:@"cutHand" ofType:@"mp4"];
+    self.moviePlayerController.contentURL = [[NSURL alloc] initFileURLWithPath:moviePath];
+    [self.moviePlayerController play];
+    [self.moviePlayerController.view bringSubviewToFront:self.view];
+    
+    
+    self.logoImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
+    self.logoImage.alpha = 0.f;
+    [self.view addSubview:self.logoImage];
+    [self.logoImage mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view.mas_centerX);
         make.top.equalTo(self.view.mas_top).offset([self Suit:220/2]);
         make.width.height.mas_equalTo([self Suit:227/2]);
     }];
     
-    UIButton *wechatBtn = [[UIButton alloc] init];
-    [wechatBtn setImage:[UIImage imageNamed:@"wechat_login"] forState:UIControlStateNormal];
-    [wechatBtn addTarget:self action:@selector(clickWechatBtn) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:wechatBtn];
-    [wechatBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.bottom.equalTo(self.view.mas_bottom).offset([self Suit:-100]);
-        make.right.equalTo(self.view.mas_centerX).offset([self Suit:-30]);
-    }];
+    self.wechatBtn = [[UIButton alloc] initWithFrame:CGRectMake(ScreenWidth/2-[self Suit:95], ScreenHeight-[self Suit:65], [self Suit:65], [self Suit:65])];
+    self.wechatBtn.alpha = 0.f;
+    [self.wechatBtn setImage:[UIImage imageNamed:@"wechat_login"] forState:UIControlStateNormal];
+    [self.wechatBtn addTarget:self action:@selector(clickWechatBtn) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.wechatBtn];
     
-    UIButton *phoneBtn = [[UIButton alloc] init];
-    [phoneBtn setImage:[UIImage imageNamed:@"phone_login"] forState:UIControlStateNormal];
-    [phoneBtn addTarget:self action:@selector(clickPhoneBtn) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:phoneBtn];
-    [phoneBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.bottom.equalTo(self.view.mas_bottom).offset([self Suit:-100]);
-        make.left.equalTo(self.view.mas_centerX).offset([self Suit:30]);
-    }];
+    self.phoneBtn = [[UIButton alloc] initWithFrame:CGRectMake(ScreenWidth/2+[self Suit:30], ScreenHeight-[self Suit:65], [self Suit:65], [self Suit:65])];
+    self.phoneBtn.alpha = 0.f;
+    [self.phoneBtn setImage:[UIImage imageNamed:@"phone_login"] forState:UIControlStateNormal];
+    [self.phoneBtn addTarget:self action:@selector(clickPhoneBtn) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.phoneBtn];
     
-    UILabel *loginLabel = [UILabel labelWithTitle:@"使用以下方式登录" AndColor:@"ffffff" AndFont:[self SuitFont:14] AndAlignment:NSTextAlignmentCenter];
-    [self.view addSubview:loginLabel];
-    [loginLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    self.loginLabel = [UILabel labelWithTitle:@"使用以下方式登录" AndColor:@"ffffff" AndFont:[self SuitFont:14] AndAlignment:NSTextAlignmentCenter];
+    self.loginLabel.alpha = 0.f;
+    [self.view addSubview:self.loginLabel];
+    [self.loginLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view.mas_centerX);
-        make.bottom.equalTo(wechatBtn.mas_top).offset([self Suit:-20]);
+        make.bottom.equalTo(self.view.mas_bottom).offset([self Suit:-185]);
     }];
     
-    UILabel *slogen = [UILabel labelWithTitle:@"这是一条slogen" AndColor:@"ffffff" AndFont:[self SuitFont:30] AndAlignment:NSTextAlignmentCenter];
-    [self.view addSubview:slogen];
-    [slogen mas_makeConstraints:^(MASConstraintMaker *make) {
+    self.slogen = [UILabel labelWithTitle:@"这是一条slogen" AndColor:@"ffffff" AndFont:[self SuitFont:30] AndAlignment:NSTextAlignmentCenter];
+    self.slogen.alpha = 0.f;
+    [self.view addSubview:self.slogen];
+    [self.slogen mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view.mas_centerX);
-        make.top.equalTo(logoImage.mas_bottom).offset([self Suit:20]);
+        make.top.equalTo(self.logoImage.mas_bottom).offset([self Suit:20]);
     }];
     
+    [self performSelector:@selector(startAnimation) withObject:self afterDelay:0.5];
 }
+
+- (void)startAnimation {
+    [UIView animateWithDuration:1.5 animations:^{
+        self.logoImage.alpha = 1.0;
+        self.slogen.alpha = 1.0;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:1 delay:0. usingSpringWithDamping:0.6 initialSpringVelocity:0.6 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.loginLabel.alpha = 1.0;
+            self.wechatBtn.alpha = 1.0;
+            self.phoneBtn.alpha = 1.0;
+            self.wechatBtn.frame = CGRectMake(ScreenWidth/2-[self Suit:95], ScreenHeight-[self Suit:165], [self Suit:65], [self Suit:65]);
+            self.phoneBtn.frame = CGRectMake(ScreenWidth/2+[self Suit:30], ScreenHeight-[self Suit:165], [self Suit:65], [self Suit:65]);
+        } completion:nil];
+    }];
+}
+
+#pragma mark - NSNotificationCenter
+- (void)playbackStateChanged
+{
+    MPMoviePlaybackState playbackState = [self.moviePlayerController playbackState];
+    if (playbackState == MPMoviePlaybackStateStopped || playbackState == MPMoviePlaybackStatePaused) {
+        [self.moviePlayerController play];
+    }
+}
+
+#pragma mark - setter and getter
+- (MPMoviePlayerController *)moviePlayerController {
+    if (!_moviePlayerController) {
+        _moviePlayerController = [[MPMoviePlayerController alloc] init];
+        _moviePlayerController.movieSourceType = MPMovieSourceTypeFile;
+        _moviePlayerController.controlStyle = MPMovieControlStyleNone;
+        _moviePlayerController.view.frame = [UIScreen mainScreen].bounds;
+        [_moviePlayerController setFullscreen:YES];
+        [_moviePlayerController setShouldAutoplay:YES];
+        [_moviePlayerController setRepeatMode:MPMovieRepeatModeOne];
+        [self.view addSubview:self.moviePlayerController.view];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playbackStateChanged) name:MPMoviePlayerLoadStateDidChangeNotification object:nil];
+        
+    }
+    return _moviePlayerController;
+}
+
 
 - (void)clickWechatBtn {
     MainTabBarController *mainVC = [[MainTabBarController alloc] init];
